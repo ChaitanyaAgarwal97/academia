048a3f9b9a9dd44da4d3fedd1df2ae0098713dbc
ChaitanyaAgarwal97 <chaitanyaagarwal97@gmail.com>
2022-09-06 12:33:26 +0530

    setup and dependencies for login-signup added

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ const PORT = process.env.PORT ?? 3000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, './public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
-const indexRoutes = require("./router/indexRoutes");
+const indexRoutes = require("./src/home/index.routes");
+const userRoutes = require("./src/user/user.routes");
 
 app.use(indexRoutes);
+app.use("/user", userRoutes);
 
 //  App listener
 app.listen(PORT, () => {
