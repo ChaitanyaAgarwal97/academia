42a09d0c5451667bb9bb52cde3a6f228a6bf1af7
ChaitanyaAgarwal97 <chaitanyaagarwal97@gmail.com>
2022-09-24 01:41:33 +0530

    Minor: Error page image updated and status code for page not found corrected to 404

--- src/index.routes.js
+++ src/index.routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 let router = express.Router();
 
 router.get("*", (req, res) => {
-    res.status(400).render("common/error", {
+    res.status(404).render("common/error", {
         error: "Error 404! Page not Found.",
     });
 });
