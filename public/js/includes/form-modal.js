a78a67a91274bafb0136c8292ca8de27308703d9
ChaitanyaAgarwal97 <chaitanyaagarwal97@gmail.com>
2022-10-14 11:04:16 +0530

    Feature: Create classroom form frontend

---
+++ public/js/includes/form-modal.js
@@ -0,0 +1,27 @@
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let createClassBtns = document.getElementsByClassName("createClass");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+Array.from(createClassBtns).forEach(ele => {
+     ele.onclick = function() {
+        modal.style.display = "block";
+    }
+});
+
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
