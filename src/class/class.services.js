762945ed0fd03b9c9a650841e3041add121f4bf6
ChaitanyaAgarwal97 <chaitanyaagarwal97@gmail.com>
2022-10-15 10:12:08 +0530

    Feature: Join class backend

--- src/class/class.services.js
+++ src/class/class.services.js
@@ -2,7 +2,7 @@ require("../../config/db.config");
 
 const Class = require("./class.model");
 const { HttpResponseError } = require("../utils/error.helper");
-const { addOwnedClass } = require("../user/user.services");
+const { addOwnedClass, addJoinedClass } = require("../user/user.services");
 
 async function create(classObj) {
     try {
@@ -22,6 +22,45 @@ async function create(classObj) {
     }
 }
 
+async function join(obj) {
+    try {
+        if(!obj.code) throw new HttpResponseError({
+            httpCode: 400,
+            msg: "Please fill in all details",
+        });
+        
+        let theClass = await Class.findOne({ code: obj.code });
+        
+        if (!theClass) throw new HttpResponseError({
+            httpCode: 400,
+            msg: "There is no class with this code",
+        });
+
+        if (theClass.owner.equals(obj.student._id)) throw new HttpResponseError({
+            httpCode: 400,
+            msg: "You are teacher of this class",
+        });
+
+        theClass.students.forEach(ele => {
+            if (ele.equals(obj.student._id)) throw new HttpResponseError({
+                httpCode: 400,
+                msg: "You are already in this class",
+            });
+        });
+
+        theClass.students.push(obj.student._id);
+
+        await theClass.save();
+
+        await addJoinedClass(obj.student, theClass._id);
+
+        return theClass;
+    } catch(err) {
+        throw err;
+    }
+}
+
 module.exports = {
     create,
+    join,
 }
