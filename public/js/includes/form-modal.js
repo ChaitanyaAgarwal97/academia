a2309aeb0437039f5bece050c2197cfd7b5f40e3
ChaitanyaAgarwal97 <chaitanyaagarwal97@gmail.com>
2022-10-15 10:11:30 +0530

    Feature: Join class frontend & Some refactoring done

--- public/js/includes/form-modal.js
+++ public/js/includes/form-modal.js
@@ -1,27 +1,44 @@
-let modal = document.getElementById("myModal");
+let createClassModal = document.getElementById("createClassModal");
+
+let joinClassModal = document.getElementById("joinClassModal");
+
+let joinClassBtns = document.getElementsByClassName("joinClass");
 
 // Get the button that opens the modal
 let createClassBtns = document.getElementsByClassName("createClass");
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+let closeBtn = document.getElementsByClassName("close");
+
+let modals = document.getElementsByClassName("modal");
 
 // When the user clicks the button, open the modal 
 Array.from(createClassBtns).forEach(ele => {
      ele.onclick = function() {
-        modal.style.display = "block";
+      joinClassModal.style.display = "none";
+      createClassModal.style.display = "block";
     }
 });
 
+Array.from(joinClassBtns).forEach(ele => {
+  ele.onclick = function() {
+    createClassModal.style.display = "none";
+    joinClassModal.style.display = "block";
+ }
+});
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+Array.from(closeBtn).forEach(ele => {
+  ele.onclick = function(event) {
+    Array.from(modals).forEach(modal => {
+      modal.style.display = "none";
+    });
+  }
+});
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == createClassModal || event.target == joinClassModal) {
+    event.target.style.display = "none";
   }
 }
